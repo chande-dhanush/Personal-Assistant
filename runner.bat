e3be7ecdf47aae7e133f37476d36123e650420c4
Dhanush C P <119944896+chande-dhanush@users.noreply.github.com>
2025-08-25 18:52:55 +0530

    Delete runner.bat

--- runner.bat
+++
@@ -1,6 +0,0 @@
-cd /d D:\sakura_modifs
-set SPOTIFY_CLIENT_ID=ca4dff3572f64335b1c919754497a3d1
-set SPOTIFY_CLIENT_SECRET=5c73d0c0661d49b29d9e97ae03d2f0ba
-set GROQ_API_KEY=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
-set GOOGLE_API_KEY=AIzaSyDP7H4sffgpp39M2zWC1NTm8Qhly12f0sc
-"D:\sakura_modifs\.venv\Scripts\pythonw.exe" "D:\sakura_modifs\run_sakura.py"
